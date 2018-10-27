6138bb0e92cf45f0a6d8d202adbd1c98b86c6e4e
YoussefAlTabei <42784337+YoussefAlTabei@users.noreply.github.com>
2018-10-27 21:30:23 +0200

    Add files via upload

---
+++ US&PW.py
@@ -0,0 +1,9 @@
+UN='myemail.com'
+PW='password'
+IUN=input('enter your email: ')
+IPW=input('enter your password: ')
+if UN == IUN and PW == IPW :
+    print('welcome back in your account!')
+else:
+    print('email or password is incoreccet try again')
+    
